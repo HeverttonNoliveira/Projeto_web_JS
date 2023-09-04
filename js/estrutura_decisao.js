67b1f477edd9dfe50fb0e3ed26b6d1821b553e6a
HeverttonNoliveira <oliveirahn.nunes@gmail.com>
2023-09-04 11:54:07 -0300

    primeiro commit

---
+++ js/estrutura_decisao.js
@@ -0,0 +1,27 @@
+//estrutura de decisão
+let a =10
+let b = 20
+
+// if estrutura basica
+if(b>a){
+    console.log("Entrou na estrutura")
+}
+
+//if aninhado
+
+// if(true){
+//     if(true){
+//         if(true){
+//             console.log("Entrou na estrutura")
+//         }
+//     }
+// }
+
+if(a<b){
+    if(b>a){
+        if(a!=b){
+            console.log("Entrou na estrutura")
+        }
+    }
+}
+
