5a05ebace2e7cb3ad8ed3b6f827b7c4bf8bc85de
HeverttonNoliveira <oliveirahn.nunes@gmail.com>
2023-09-05 11:18:28 -0300

    primeiro commit

--- js/repetical.js
+++ js/repetical.js
@@ -6,3 +6,20 @@ while(contador <10){
     contador++
     console.log(`O contador vale ${contador} vcs viram`)
 }
+
+// do while
+
+let cont = 0
+
+do{
+
+    cont++
+    console.log("Executando do while...")
+}while(cont <=10)
+
+//For
+let i
+
+for(i=0; i<10; i++){
+    console.log(`tyftf ${i}`)
+}
