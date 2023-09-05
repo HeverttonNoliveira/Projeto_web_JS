1fc2534b4b6fea30e54873043550790e88a0d758
HeverttonNoliveira <oliveirahn.nunes@gmail.com>
2023-09-05 09:32:28 -0300

    primeiro commit

--- js/estrutura_decisao.js
+++ js/estrutura_decisao.js
@@ -1,10 +1,12 @@
 //estrutura de decisão
+// if(){}
+
 let a =10
 let b = 20
 
 // if estrutura basica
 if(b>a){
-    console.log("Entrou na estrutura")
+    console.log("É verdadeiro")
 }
 
 //if aninhado
@@ -20,8 +22,42 @@ if(b>a){
 if(a<b){
     if(b>a){
         if(a!=b){
-            console.log("Entrou na estrutura")
+            console.log("É verdadeiro")
         }
     }
 }
+
+// Dia 05/09
+
+let c = 30
+let d = 40
+
+if(d>c){
+    console.log("É verdadeiro")
+}else{
+    console.log("É falso")
+}
+
+//Utilizando else if
+let saudacao, periodo;
+periodo = 10;
+
+console.log(`Digite: \n 1-bom dia \n 2-boa tarde \n 3-boa noite`);
+
+if(periodo === 1){
+    saudacao = "bom dia";
+}else if(periodo === 2){
+    saudacao = "boa tarde";
+}else if(periodo === 3){
+    saudacao = "boa noite";
+}else{
+    saudacao = "Nenhum periodo selecionado";
+}
+
+console.log(`E ai, ${saudacao}`);
+
+
+
+
+
 
