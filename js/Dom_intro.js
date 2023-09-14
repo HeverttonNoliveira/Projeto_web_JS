1992d5bb9fdc0d32dd2ea347a4cca934b5fe141d
HeverttonNoliveira <oliveirahn.nunes@gmail.com>
2023-09-14 11:59:13 -0300

    primeiro commit

---
+++ js/Dom_intro.js
@@ -0,0 +1,10 @@
+// Trabalhando com DOM
+
+document.getElementById("exemplo").innerHTML = "Olá word ";
+document.getElementsByClassName("teste") = "Teste";
+
+
+function getcubo(){
+    let number = document.getElementById("number").value;
+    alert(number*number*number);
+}
--- js/calculadora2.js
+++ js/calculadora2.js
@@ -1,22 +1,21 @@
-
-
+//Obejeto
 const calculadora = {
 	num1: 10.5,
 	num2: 20.5,
 
 	soma: (v1, v2) => {
 		this.num1 = v1;
-		this.num1 = v2;
-		return (this.num1 + this.num2)
+		this.num2 = v2;
+		return this.num1 + this.num2;
 	},
 	sub: (v1, v2) => {
 		this.num1 = v1;
-		this.num1 = v2;
+		this.num2 = v2;
 		return this.num1 - this.num2;
 	},
-	div: (num1, num2) => {
-		this.num1 = num1;
-		this.num1 = num2;
+	div: (v1, v2) => {
+		this.num1 = v1;
+		this.num2 = v2;
 
 		if (num2 === 0) {
 			return false;
@@ -24,19 +23,19 @@ const calculadora = {
 			return this.num1 / this.num2;
 		}
 	},
-	mult: (num1, num2) => {
-		this.num1 = num1;
-		this.num1 = num2;
+	mult: (v1, v2) => {
+		this.num1 = v1;
+		this.num2 = v2;
 		return this.num1 * this.num2;
-	}
+	},
 };
 
 //Calculadora
 let v1, v2;
-let op,operacao, rep = 0;
+let op,operacao,rep = 0;
 
 v1 = 10;
-v2 = 20;
+v2 = 10;
 operacao = 1;
 
 console.log("Calculadora");
@@ -45,16 +44,28 @@ console.log("Operação: \n\n1-Soma \n2-Subtração \n3-Multiplicação \n4-Divi
 
 switch (operacao) {
 	case 1:
-		rep = calculadora.soma(v1,v2);
+		rep = calculadora.soma(v1, v2);
+		op = "soma"
 		break;
 	case 2:
-		rep = calculadora.sub(v1,v2);
+		rep = calculadora.sub(v1, v2);
+		op = "subtração"
 		break;
 	case 3:
-		rep = calculadora.mult(v1,v2);
+		rep = calculadora.mult(v1, v2);
+		op = "multiplicação"
 		break;
 	case 4:
-		rep = calculadora.div(v1,v2);
+		rep = calculadora.div(v1, v2);
+		op = 'divisão'
 }
 
-console.log(`Números digitados ${v1}, ${v2} \nO resultado: ${rep}`);
+console.log(`Números digitados ${v1},${v2}\n Operação:${op} \nO resultado: ${rep}`);
+
+function somavalor(){
+	let numm1 = document.getElementById("n1").value;
+	let numm2 = document.getElementById("n2").value;
+	let soma1 = numm1 + numm2
+	alert(soma1);
+}
+
