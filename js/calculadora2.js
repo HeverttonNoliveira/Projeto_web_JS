042840edaf99024d95cca1bc3ba12e99c374adef
HeverttonNoliveira <oliveirahn.nunes@gmail.com>
2023-09-15 12:07:28 -0300

    primeiro commit

--- js/calculadora2.js
+++ js/calculadora2.js
@@ -1,3 +1,13 @@
+//Variáveis
+let v1, v2;
+let operacao, rep = 0
+
+v1 = document.getElementById("num1").value
+v2 = document.getElementById("num2").value
+
+
+operacao = document.getElementById("ope").value
+
 //Obejeto
 const calculadora = {
 	num1: 10.5,
@@ -31,41 +41,22 @@ const calculadora = {
 };
 
 //Calculadora
-let v1, v2;
-let op,operacao,rep = 0;
 
-v1 = 10;
-v2 = 10;
-operacao = 1;
-
-console.log("Calculadora");
-console.log("Insira dos números e escolha a operação desejada.");
-console.log("Operação: \n\n1-Soma \n2-Subtração \n3-Multiplicação \n4-Divisão\n");
-
-switch (operacao) {
-	case 1:
-		rep = calculadora.soma(v1, v2);
-		op = "soma"
-		break;
-	case 2:
-		rep = calculadora.sub(v1, v2);
-		op = "subtração"
-		break;
-	case 3:
-		rep = calculadora.mult(v1, v2);
-		op = "multiplicação"
-		break;
-	case 4:
-		rep = calculadora.div(v1, v2);
-		op = 'divisão'
+function resp(){
+	switch (operacao) {
+		case 1:
+			rep = calculadora.soma(v1, v2);
+			break;
+		case 2:
+			rep = calculadora.sub(v1, v2);
+			break;
+		case 3:
+			rep = calculadora.mult(v1, v2);
+			break;
+		case 4:
+			rep = calculadora.div(v1, v2);
+			break;
+	}
 }
-
-console.log(`Números digitados ${v1},${v2}\n Operação:${op} \nO resultado: ${rep}`);
-
-function somavalor(){
-	let numm1 = document.getElementById("n1").value;
-	let numm2 = document.getElementById("n2").value;
-	let soma1 = numm1 + numm2
-	alert(soma1);
-}
-
+document.getElementById("resultado").innerHTML = rep
+console.log(v1)
